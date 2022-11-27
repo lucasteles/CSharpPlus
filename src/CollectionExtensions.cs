afdee96ebb7cdcd5104a1adb2327d37019ea4f5e
Lucas Teles <lucas.teles@outlook.com>
2022-11-27 16:33:25 -0300

    remove interactive extensions dependency

--- src/CollectionExtensions.cs
+++ src/CollectionExtensions.cs
@@ -11,10 +11,18 @@ public static class CollectionExtensions
     /// <summary>
     /// Adds the elements of the specified collection to the end of the IList of T.
     /// </summary>
-    public static void AddRange<T>(this IList<T> self, IEnumerable<T> items) => items.ForEach(self.Add);
+    public static void AddRange<T>(this IList<T> self, IEnumerable<T> items)
+    {
+        foreach (var item in items)
+            self.Add(item);
+    }
 
     /// <summary>
     /// Adds the elements of the specified collection to the end of the ICollection of T.
     /// </summary>
-    public static void AddRange<T>(this ICollection<T> self, IEnumerable<T> items) => items.ForEach(self.Add);
+    public static void AddRange<T>(this ICollection<T> self, IEnumerable<T> items)
+    {
+        foreach (var item in items)
+            self.Add(item);
+    }
 }
