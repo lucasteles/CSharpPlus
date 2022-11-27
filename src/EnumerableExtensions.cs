efcb98ff073e36b29869c71fdef5e2fc1b2a9195
Lucas Teles <lucas.teles@outlook.com>
2022-11-27 14:37:32 -0300

    WIP

--- src/EnumerableExtensions.cs
+++ src/EnumerableExtensions.cs
@@ -12,7 +12,8 @@ public static class EnumerableExtensions
     /// </summary>
     /// <param name="values"></param>
     /// <param name="separator"></param>
-    public static string JoinString(this IEnumerable<string> values, char separator) => string.Join(separator, values);
+    public static string JoinString(this IEnumerable<string> values, char separator) =>
+        string.Join(separator, values);
 
     /// <summary>
     /// Concatenates the members of a collection, using the specified separator between each member.
@@ -27,14 +28,16 @@ public static class EnumerableExtensions
     /// </summary>
     /// <param name="values"></param>
     /// <param name="separator"></param>
-    public static string JoinString(this IEnumerable<char> values, char separator) => string.Join(separator, values);
+    public static string JoinString(this IEnumerable<char> values, char separator) =>
+        string.Join(separator, values);
 
     /// <summary>
     /// Concatenates the members of a collection, using the specified separator between each member.
     /// </summary>
     /// <param name="values"></param>
     /// <param name="separator"></param>
-    public static string JoinString(this IEnumerable<char> values, string separator) => string.Join(separator, values);
+    public static string JoinString(this IEnumerable<char> values, string separator) =>
+        string.Join(separator, values);
 
 
     /// <summary>
@@ -84,7 +87,8 @@ public static class EnumerableExtensions
     /// <summary>
     /// Returns the minimum and maximum value in a generic sequence by key member.
     /// </summary>
-    public static (T? Min, T? Max) MinMaxBy<T, TProp>(this IEnumerable<T> @this, Func<T, TProp> keySelector)
+    public static (T? Min, T? Max) MinMaxBy<T, TProp>(this IEnumerable<T> @this,
+        Func<T, TProp> keySelector)
     {
         var items = @this.ToArray();
         return (items.MinBy(keySelector), items.MaxBy(keySelector));
@@ -145,7 +149,8 @@ public static class EnumerableExtensions
     /// <param name="t3"></param>
     /// <typeparam name="T"></typeparam>
     /// <exception cref="InvalidOperationException">The source sequence dont have 4 or more items</exception>
-    public static void Deconstruct<T>(this IEnumerable<T> enumerable, out T t0, out T t1, out T t2, out T t3)
+    public static void Deconstruct<T>(this IEnumerable<T> enumerable, out T t0, out T t1, out T t2,
+        out T t3)
     {
         var items = TryGetFromEnumerable(enumerable, 4);
         (t0, t1, t2, t3) = (items[0], items[1], items[2], items[3]);
@@ -162,7 +167,8 @@ public static class EnumerableExtensions
     /// <param name="t4"></param>
     /// <typeparam name="T"></typeparam>
     /// <exception cref="InvalidOperationException">The source sequence dont have 5 or more items</exception>
-    public static void Deconstruct<T>(this IEnumerable<T> enumerable, out T t0, out T t1, out T t2, out T t3, out T t4)
+    public static void Deconstruct<T>(this IEnumerable<T> enumerable, out T t0, out T t1, out T t2,
+        out T t3, out T t4)
     {
         var items = TryGetFromEnumerable(enumerable, 5);
         (t0, t1, t2, t3, t4) = (items[0], items[1], items[2], items[3], items[4]);
@@ -183,7 +189,7 @@ public static class EnumerableExtensions
     /// <param name="enumerable"></param>
     /// <typeparam name="T"></typeparam>
     /// <returns></returns>
-    public static IEnumerable<T> NotNull<T>(this IEnumerable<T?> enumerable) where T : class =>
+    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> enumerable) where T : class =>
         enumerable.Where(e => e is not null).Cast<T>();
 
     /// <summary>
@@ -192,7 +198,17 @@ public static class EnumerableExtensions
     /// <param name="enumerable"></param>
     /// <typeparam name="T"></typeparam>
     /// <returns></returns>
-    public static IEnumerable<T> NotNull<T>(this IEnumerable<T?> enumerable) where T : struct =>
+    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> enumerable)
+        where T : struct =>
         enumerable.Where(e => e is not null).Select(e => e!.Value);
 
+
+    /// <summary>
+    /// Enumerate the enumerable to a collection of value tuples (int Index, T Value)
+    /// </summary>
+    /// <param name="enumerable"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static IEnumerable<(int Index, T Value)> Enumerate<T>(IEnumerable<T> enumerable) =>
+        enumerable.Select((value, index) => (index, value));
 }
