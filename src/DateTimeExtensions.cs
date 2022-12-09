359e162377db34f66c1aae50d1f5fcef0ae241f5
Lucas Teles <lucas.teles@outlook.com>
2022-12-09 17:14:42 -0300

    date and time only extensions

--- src/DateTimeExtensions.cs
+++ src/DateTimeExtensions.cs
@@ -9,8 +9,18 @@ public static class DateTimeExtensions
     /// <summary>
     /// Format date as default System.Text.Json format
     /// </summary>
-    /// <param name="dateTime"></param>
-    /// <returns></returns>
     public static string ToJsonString(this DateTime dateTime) =>
         JsonSerializer.Serialize(dateTime)[1..^1];
+
+    /// <summary>
+    /// Returns a DateOnly instance that is set to the date part of the specified dateTime.
+    /// </summary>
+    public static DateOnly ToDateOnly(this DateTime dateTime) =>
+        DateOnly.FromDateTime(dateTime);
+
+    /// <summary>
+    /// Constructs a TimeOnly object from a DateTime representing the time of the day in this DateTime object
+    /// </summary>
+    public static TimeOnly ToTimeOnly(this DateTime dateTime) =>
+        TimeOnly.FromDateTime(dateTime);
 }
