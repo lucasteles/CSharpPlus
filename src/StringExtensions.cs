af915b0aa0a8b2bacf303d570e1b64e71a7901c1
Lucas Teles <lucas.teles@outlook.com>
2022-12-29 16:55:54 -0300

    digit only extension

--- src/StringExtensions.cs
+++ src/StringExtensions.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics.Contracts;
+using System.Linq;
 
 /// <summary>
 /// String extensions
@@ -12,7 +13,8 @@ public static class StringExtensions
     /// <param name="value">The string to test</param>
     /// <returns>true if the value parameter is null or an empty string (""); otherwise, false.</returns>
     [Pure]
-    public static bool IsNullOrEmpty([NotNullWhen(false)] this string? value) => string.IsNullOrEmpty(value);
+    public static bool IsNullOrEmpty([NotNullWhen(false)] this string? value) =>
+        string.IsNullOrEmpty(value);
 
     /// <summary>
     /// Indicates whether a specified string is null, empty, or consists only of white-space characters.
@@ -21,7 +23,20 @@ public static class StringExtensions
     /// <returns> true if the value parameter is null or Empty, or if value consists exclusively of white-space characters.
     /// </returns>
     [Pure]
-    public static bool IsNullOrWhiteSpace([NotNullWhen(false)] this string? value) => string.IsNullOrWhiteSpace(value);
+    public static bool IsNullOrWhiteSpace([NotNullWhen(false)] this string? value) =>
+        string.IsNullOrWhiteSpace(value);
 
+    /// <summary>
+    /// Remove all non-digits characters
+    /// </summary>
+    [Pure]
+    public static string RemoveNonDigit(this string value) =>
+        new(value.Where(char.IsDigit).ToArray());
 
+    /// <summary>
+    /// Remove all non digit or letter characters
+    /// </summary>
+    [Pure]
+    public static string RemoveNonDigitOrLetter(this string value) =>
+        new(value.Where(char.IsLetterOrDigit).ToArray());
 }
