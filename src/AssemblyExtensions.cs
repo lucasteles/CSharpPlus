37a67510a0a6c69abf7f50637fb74165d3506546
Lucas Teles <lucas.teles@outlook.com>
2023-04-25 20:13:57 -0300

    assembly extensions

---
+++ src/AssemblyExtensions.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace CSharpPlus;
+
+/// <summary>
+/// Assembly Extensions
+/// </summary>
+public static class AssemblyExtensions
+{
+    /// <summary>
+    /// Get all non-abstract implementations of T
+    /// </summary>
+    /// <param name="assembly"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static IEnumerable<Type> GetAllImplementations<T>(this Assembly assembly)
+        where T : notnull
+    {
+        var type = typeof(T);
+        return assembly.GetTypes().Where(p =>
+            type.IsAssignableFrom(p) && p is
+                {IsInterface: false, IsAbstract: false, IsGenericType: false});
+    }
+
+    /// <summary>
+    /// Instantiate all non-abstract implementations of T with a public parameterless constructor
+    /// </summary>
+    /// <param name="assembly"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static IEnumerable<T> InstantiateAllImplementations<T>(this Assembly assembly)
+        where T : notnull =>
+        assembly
+            .GetAllImplementations<T>()
+            .Select(t => t
+                .IsValueType
+                ? Activator.CreateInstance(t)
+                : t.GetConstructor(Type.EmptyTypes)
+                    ?.Invoke(Array.Empty<object>()))
+            .NotNullOnly()
+            .Cast<T>();
+}
---
+++ tests/CSharpPlus.Tests/AssemblyExtensionsTests.cs
@@ -0,0 +1,85 @@
+using System.Reflection;
+using Bogus.Platform;
+
+namespace CSharpPlus.Tests;
+
+public class AssemblyExtensionsTests
+{
+    static readonly Assembly assembly = typeof(AssemblyExtensionsTests).GetAssembly();
+
+    static readonly Type[] expectedTypes =
+    {
+        typeof(TestStruct),
+        typeof(TestRecord),
+        typeof(TestClass),
+        typeof(TestClass2),
+        typeof(TestClass3),
+        typeof(TestClass4),
+    };
+
+    [Test]
+    public void ShouldGetCorrectTypes()
+    {
+        var types = assembly.GetAllImplementations<ITestInterface>();
+
+        types.Should().BeEquivalentTo(
+            expectedTypes.Append(typeof(TestClassCtor)),
+            opt => opt.WithoutStrictOrdering());
+    }
+
+    [Test]
+    public void ShouldInstantiateAllPossibleTypes()
+    {
+        var types = assembly
+            .InstantiateAllImplementations<ITestInterface>()
+            .Select(x => x.GetType());
+
+        types.Should().BeEquivalentTo(expectedTypes, opt => opt.WithoutStrictOrdering());
+    }
+}
+
+interface ITestInterface
+{
+}
+
+class TestClass : ITestInterface
+{
+}
+
+struct TestStruct : ITestInterface
+{
+}
+
+record TestRecord : ITestInterface;
+
+class TestClassCtor : ITestInterface
+{
+    readonly string value;
+    public TestClassCtor(string value) => this.value = value;
+}
+
+#pragma warning disable S2326
+class TestClassBad2<T> : ITestInterface
+#pragma warning restore S2326
+{
+}
+
+interface ITestOtherInterface : ITestInterface
+{
+}
+
+class TestClass2 : ITestOtherInterface
+{
+}
+
+class TestClass3 : TestClassBad2<int>
+{
+}
+
+abstract class TestAbstract : ITestInterface
+{
+}
+
+class TestClass4 : TestAbstract
+{
+}
